09cb4aa1ee4ce9f7540fbd8de21b7a85aab6b19e
John Carveth <jlcarveth@gmail.com>
2023-06-16 11:48:35 -0400

    Visual changes

---
+++ components/Navbar.tsx
@@ -0,0 +1,22 @@
+export default function Navbar() {
+  return (
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <ul class="nav nav-pills ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="https://github.com/JLCarveth">
+              <i class="bi-git me-2" role="img" aria-label="Git" />
+              <span class="d-md-inline">GitHub</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://blog.jlcarveth.dev">
+              <i class="bi-journals me-2" role="img" aria-label="Blog" />
+              <span class="d-md-inline">Blog</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
