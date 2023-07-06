46e2c04a21991b5eacfc117ddfbf75a077a1d9ca
John Carveth <jlcarveth@gmail.com>
2023-07-06 12:40:44 -0400

    Plausible Analytics

--- components/Head.tsx
+++ components/Head.tsx
@@ -4,11 +4,15 @@ export default function Head() {
   return (
     <IHead>
       <title>🦕jlcarveth.dev🦕</title>
-      <meta charset="utf-8"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1"/>
-      <link rel="stylesheet" href="/css/bootstrap.min.css"/>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"/>
-      <link rel="stylesheet" href="/css/styles.css"/>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="stylesheet" href="/css/bootstrap.min.css" />
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
+      />
+      <link rel="stylesheet" href="/css/styles.css" />
+      <script defer src="/js/script.js" data-domain="jlcarveth.dev" />
     </IHead>
   );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,10 +1,7 @@
 import Head from "../components/Head.tsx";
 import Navbar from "../components/Navbar.tsx";
-import Sidebar from "../components/Sidebar.tsx";
 import Title from "../islands/title.tsx";
 import Deployment from "../components/Deployment.tsx";
-import Services from "../components/Services.tsx";
-import Welcome from "../components/Welcome.tsx";
 
 export default function Home() {
   return (
@@ -15,19 +12,23 @@ export default function Home() {
           defer
           src="/js/script.js"
           data-domain="jlcarveth.dev"
-        ></script>
+        />
       </Head>
-      <Navbar />
+      <Navbar/>
       <div class="overlay">
-        <main class="container-fluid ms-0 ps-0">
-          <div class="px-4 py-4 my-5">
-            <Welcome />
-            <hr />
-            <Deployment />
-            <hr />
-            <Services />
+      <main class="container-fluid ms-0 ps-0">
+        <div class="row">
+          <div class="col">
+            <div class="px-4 py-4 my-5 text-center">
+              <h1 class="display-5 fw-bold">Welcome to my website!</h1>
+              <h3>My name is John Carveth, a <Title/></h3>
+              <p class="py-4">I am from <mark class="bg-success bg-opacity-25">Toronto, ON</mark> and have been writing code for over 10 years.</p>
+              <hr/>
+              <Deployment />
+            </div>
           </div>
-        </main>
+        </div>
+      </main>
       </div>
     </>
   );
